859d2aec1fcd84f4c95edecb0d9d9a50ac26d211
ifetreaty <68975552+ifetreaty@users.noreply.github.com>
2024-10-01 18:21:14 +0100

    fix bundler

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -5,28 +5,43 @@ import commonjs from "@rollup/plugin-commonjs"; //handles ES6 features
 import typescript from "@rollup/plugin-typescript";
 import postcss from "rollup-plugin-postcss"; //handles css imports
 import path from "path";
+import babel from "@rollup/plugin-babel";
 import { terser } from "rollup-plugin-terser";
 import copy from "rollup-plugin-copy";
+import serve from "rollup-plugin-serve";
+import livereload from "rollup-plugin-livereload";
+import replace from "@rollup/plugin-replace";
 
 export default {
   input: "src/main.tsx",
   output: [
     {
       file: "dist/bundle.js",
-      format: "iife",
+      format: "cjs",
       sourcemap: true,
     },
   ],
   plugins: [
     peerDepsExternal(), // Exclude peer dependencies
+    resolve({
+      extensions: [".js", ".jsx", ".ts", ".tsx"], // Resolve these extensions
+      browser: true,
+      dedupe: ["react", "react-dom"],
+    }),
     typescript({
       // TypeScript integration
       tsconfig: "./tsconfig.app.json",
     }),
-    resolve({
-      extensions: [".js", ".jsx", ".ts", ".tsx"], // Resolve these extensions
+    babel({
+      babelHelpers: "bundled",
+      presets: ["@babel/preset-react"],
+      extensions: [".js", ".jsx", ".ts", ".tsx"],
     }),
     commonjs(), // Convert CommonJS to ES6
+    replace({
+      "process.env.NODE_ENV": JSON.stringify("development"),
+      preventAssignment: true,
+    }),
     postcss({
       extensions: [".css"], // Process and bundle CSS files
       extract: path.resolve("dist/styles.css"), // CSS file output
@@ -34,6 +49,14 @@ export default {
     }),
     terser(), // Minify JS bundle
     copy({ targets: [{ src: "./index.html", dest: "dist" }] }),
+    serve({
+      open: true,
+      verbose: true,
+      contentBase: ["", "public"],
+      host: "localhost",
+      port: 3000,
+    }),
+    livereload({ watch: "dist" }),
   ],
-  // external: ["react", "react-dom"], // Externalize React dependencies
+  external: ["react", "react-dom"], // Externalize React dependencies
 };

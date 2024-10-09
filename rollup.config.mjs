b8ca1f07af92653697c3abb28f6e37987c525728
ugonnathelma <moncateyes@gmail.com>
2024-10-09 21:55:34 +0200

    Fix rollup

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,62 +1,53 @@
 // rollup.config.mjs
-import peerDepsExternal from "rollup-plugin-peer-deps-external";
 import resolve from "@rollup/plugin-node-resolve";
-import commonjs from "@rollup/plugin-commonjs"; //handles ES6 features
+import commonjs from "@rollup/plugin-commonjs";
 import typescript from "@rollup/plugin-typescript";
-import postcss from "rollup-plugin-postcss"; //handles css imports
-import path from "path";
 import babel from "@rollup/plugin-babel";
-import { terser } from "rollup-plugin-terser";
-import copy from "rollup-plugin-copy";
 import serve from "rollup-plugin-serve";
 import livereload from "rollup-plugin-livereload";
 import replace from "@rollup/plugin-replace";
 
+import postcss from "rollup-plugin-postcss";
+
 export default {
   input: "src/main.tsx",
-  output: [
-    {
-      file: "dist/bundle.js",
-      format: "cjs",
-      sourcemap: true,
-    },
-  ],
+  output: {
+    file: "dist/bundle.js",
+    format: "esm",
+    sourcemap: true,
+  },
   plugins: [
-    peerDepsExternal(), // Exclude peer dependencies
-    resolve({
-      extensions: [".js", ".jsx", ".ts", ".tsx"], // Resolve these extensions
-      browser: true,
-      dedupe: ["react", "react-dom"],
-    }),
-    typescript({
-      // TypeScript integration
-      tsconfig: "./tsconfig.app.json",
-    }),
-    babel({
-      babelHelpers: "bundled",
-      presets: ["@babel/preset-react"],
-      extensions: [".js", ".jsx", ".ts", ".tsx"],
-    }),
-    commonjs(), // Convert CommonJS to ES6
     replace({
       "process.env.NODE_ENV": JSON.stringify("development"),
       preventAssignment: true,
     }),
-    postcss({
-      extensions: [".css"], // Process and bundle CSS files
-      extract: path.resolve("dist/styles.css"), // CSS file output
-      minimize: true, // Minify the CSS
+    resolve({
+      browser: true,
+      preferBuiltins: true,
     }),
-    terser(), // Minify JS bundle
-    copy({ targets: [{ src: "./index.html", dest: "dist" }] }),
+    commonjs({
+      include: /node_modules/,
+    }),
+    babel({
+      babelHelpers: "bundled",
+      extensions: [".js", ".jsx", ".ts", ".tsx"],
+      babelrc: true,
+      exclude: /node_modules/,
+      presets: ["@babel/preset-react", "@babel/preset-typescript"],
+    }),
+    typescript({
+      tsconfig: "./tsconfig.app.json",
+      sourceMap: true,
+    }),
+    postcss({
+      extract: true,
+      minimize: false,
+    }),
     serve({
       open: true,
-      verbose: true,
-      contentBase: ["", "public"],
-      host: "localhost",
+      contentBase: ["."],
       port: 3000,
     }),
-    livereload({ watch: "dist" }),
+    livereload("."),
   ],
-  external: ["react", "react-dom"], // Externalize React dependencies
 };
